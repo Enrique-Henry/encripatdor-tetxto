6edc36f1f15b7c308ef5105e59b6744c3f0385e5
HenryZula <grupoanguiz@gmail.com>
2024-01-31 18:30:52 -0600

    añadiendo estructura HTML

---
+++ encriptador.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Encriptador de texto.';
+
+let textoUsuario = ('Ingrese el texto aqui.');
+let letraE = 'enter';
+let letraI = 'imes';
+let letraA = 'ai';
+let letraO = 'ober';
+let letraU = 'ufat';
