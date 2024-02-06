5b4893108e2ad90c9ea4ea6004f981e7a37d38b0
HenryZula <grupoanguiz@gmail.com>
2024-02-06 16:18:53 -0600

    añadiendo HTML

--- encriptador.js
+++ encriptador.js
@@ -1,9 +1,22 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Encriptador de texto.';
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
 
-let textoUsuario = ('Ingrese el texto aqui.');
-let letraE = 'enter';
-let letraI = 'imes';
-let letraA = 'ai';
-let letraO = 'ober';
-let letraU = 'ufat';
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi, "imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+        
+    if  (texto.length !=0){
+        document,getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto encriptado con exito";
+        parrafo.textContent = "";
+    } else {
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        swal("Ooops!", "Debes ingresar un texto", "warning");
+    }
+}
