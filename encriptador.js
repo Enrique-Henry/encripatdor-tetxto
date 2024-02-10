c6ac0d03f6fb24fb319779bd5e9be82d1265c0d3
HenryZula <grupoanguiz@gmail.com>
2024-02-10 13:57:00 -0600

    funcion desencriptar

--- encriptador.js
+++ encriptador.js
@@ -16,8 +16,27 @@ function encriptar() {
       "Texto encriptado con éxito";
       document.getElementById("parrafo").textContent = "";
   } else {
+    tituloMensaje.textContent = "Ingresa el texto que deseas encriptar o desincriptar."
     alert("Debes ingresar algun texto");
   }
 }
+function desencriptar() {
+        let texto = document.getElementById("texto").value;
 
-    
+        let textoCifrado = texto
+          .replace(/enter/gi, "e")
+          .replace(/imes/gi, "")
+          .replace(/ai/gi, "a")
+          .replace(/ober/gi, "o")
+          .replace(/ufat/gi, "u"); 
+
+          if(texto.length != 0){
+            document.getElementById("texto").value = textoCifrado;
+            document.getElementById("titulo-mensaje").value.textContent = 
+            "Texto encriptado con éxito";
+            document.getElementById("parrafo").textContent = "";
+            } else {
+            tituloMensaje.textContent = "Ingresa el texto que deseas encriptar o desincriptar."
+            alert("Debes ingresar algun texto");
+            }
+          }
