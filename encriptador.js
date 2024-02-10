19482c1693ee707c0a0c718ef60ee3b414451e4b
HenryZula <grupoanguiz@gmail.com>
2024-02-10 12:42:39 -0600

    Diseño CSS

--- encriptador.js
+++ encriptador.js
@@ -1,22 +1,23 @@
 function encriptar() {
-    let texto = document.getElementById("texto").value;
-    let tituloMensaje = document.getElementById("titulo-mensaje");
-    let parrafo = document.getElementById("parrafo");
+        let texto = document.getElementById("texto").value;
+         let tituloMensaje = document.getElementById("titulo-mensaje");
+         let parrafo = document.getElementById("parrafo");
 
-    let textoCifrado = texto
-        .replace(/e/gi, "enter")
-        .replace(/i/gi, "imes")
-        .replace(/a/gi, "ai")
-        .replace(/o/gi, "ober")
-        .replace(/u/gi, "ufat");
-        
-    if  (texto.length !=0){
-        document,getElementById("texto").value = textoCifrado;
-        tituloMensaje.textContent = "Texto encriptado con exito";
-        parrafo.textContent = "";
-    } else {
-        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
-        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
-        swal("Ooops!", "Debes ingresar un texto", "warning");
-    }
+        let textoCifrado = texto 
+            .replace(/e/gi, "enter")
+            .replace(/i/gi, "imes")
+            .replace(/a/gi, "ai")
+            .replace(/o/gi, "ober")
+            .replace(/u/gi, "ufat"); 
+
+    if(document.getElementById("texto").value.length !=0) {
+      document.getElementById("texto").value = textoCifrado;
+      document.getElementById("titulo-mensaje").value.textContent = 
+      "Texto encriptado con éxito";
+      document.getElementById("parrafo").textContent = "";
+  } else {
+    alert("Debes ingresar algun texto");
+  }
 }
+
+    
